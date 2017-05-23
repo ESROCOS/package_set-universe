ab20edbbb9758fc5625b6664bb04c995f2eb29b2
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-05-23 15:21:21 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,5 @@
+
+require 'autoproj/gitorious'
+if !Autoproj.has_source_handler? 'github'
+    Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+end
